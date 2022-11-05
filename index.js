30bf192304fc15d4758e7d2d8c075857c48b09bb
Cooper Hall <cooperhall111@gmail.com>
2022-11-05 12:54:10 -0400

    commit tree 2bbdb52d07246c7c03618cac7a2bf7e2bd76c4b8

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+const submitData = (name, email) => {
+    let formData = {
+        name: name,
+        email: email
+    };
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body:JSON.stringify(formData)
+    };
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+        let h2 = document.createElement("h2")
+        h2.innerText = object.id;
+        document.body.appendChild(h2);
+        console.log(object);
+    })
+    .catch(function(error) {
+        let h3 = document.createElement("h3")
+        h3.innerText = error;
+        document.body.appendChild(h3);
+        console.log(error);
+    })
+}
